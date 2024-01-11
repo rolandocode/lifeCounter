cf1bac4cc5605a84423e672adae4aac01eff627d
dev-rvdl <dev-rvdl@outlook.es>
2024-01-11 00:12:17 -0600

    Time mode button

--- LifeCounter/rvCounterScript.js
+++ LifeCounter/rvCounterScript.js
@@ -3,37 +3,52 @@ var milliSecondsInASecond = 1000;
 var hoursInADay = 24;
 var minutesInAnHour = 60;
 var SecondsInAMinute  = 60;
+var _newCountingMode = true;
 
 var totalSeconds = 0;
 function putDaysWithoutAlcohol(){
-    debugger;
 
     var timeDifference = new  Date().getTime() - _startDate;
 
-    var secondsToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond) % this.SecondsInAMinute);
-    var minutesToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour) % this.SecondsInAMinute);
-    var hoursToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute) % this.hoursInADay);
-    var daysToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute * this.hoursInADay));
-    var years = getYears();
+    if (_newCountingMode){
+        var secondsToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond) % this.SecondsInAMinute);
+        var minutesToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour) % this.SecondsInAMinute);
+        var hoursToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute) % this.hoursInADay);
+        var daysToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute * this.hoursInADay));
+        var years = getYears();
 
-    document.getElementById("noAlcoholDays").innerText =  daysToDday +" días";
-    document.getElementById("noAlcoholSeconds").innerText = secondsToDday +" segundos";
-    document.getElementById("noAlcoholMinutes").innerText =  minutesToDday+" minutos";
-    document.getElementById("noAlcoholHours").innerText =  hoursToDday +" horas";
+        document.getElementById("noAlcoholDays").innerText =  daysToDday +" días";
+        document.getElementById("noAlcoholSeconds").innerText = secondsToDday +" segundos";
+        document.getElementById("noAlcoholMinutes").innerText =  minutesToDday+" minutos";
+        document.getElementById("noAlcoholHours").innerText =  hoursToDday +" horas";
+    }
+    else{
+        var secondsToDday = getInSeconds();
+        var minutesToDday = getInMinutes();
+        var hoursToDday = getInHours();
+        var daysToDday = calculateDays();
+
+        document.getElementById("noAlcoholDays").innerText =  daysToDday +" días";
+        document.getElementById("noAlcoholSeconds").innerText = secondsToDday +" segundos";
+        document.getElementById("noAlcoholMinutes").innerText =  minutesToDday+" minutos";
+        document.getElementById("noAlcoholHours").innerText =  hoursToDday +" horas";
+    }
+
+
+    var years = getYears();
 
     if (years != 0)
         document.getElementById("completedYears").innerText =  "¡FELICIDADES! has arrebasado " + (years == 1 ? "un año " : years + " años ") + "sobrio";
 }
 
 var localDoggiesPhotos = [
-    // "http://drive.google.com/uc?export=view&id=1V3OCiorXnb83jaqHzPBMDwCT8jUGN3m3",
-    "http://drive.google.com/uc?id=1fSH2GhZ6VFLtiGDMmGqx2fg0Sg4GIo4q"
+    "https://onedrive.live.com/embed?resid=E530B3F2B44ACA4B%21107019&authkey=%21AAWWKDbBtKS39_A"
+    
 ]
 
 function capture() {
 
      document.getElementById("captureButton").style.display = "none"
-
     html2canvas(document.body, {
         // https://github.com/niklasvh/html2canvas/issues/722
         letterRendering: 1, allowTaint: true, useCORS: true, logging: true
@@ -46,7 +61,6 @@ function capture() {
         // https://stackoverflow.com/questions/27798126/how-to-open-the-newly-created-image-in-a-new-tab
         // var w = window.open("");
         // w.document.write(image.outerHTML);
-
         document.getElementById("captureButton").style.display = "inline"
         downloadImage(image.src);
 
@@ -58,9 +72,7 @@ async function downloadImage(
     nameOfDownload = 'my-image.png',
   ) {
     const response = await fetch(imageSrc);
-  
     const blobImage = await response.blob();
-  
     const href = URL.createObjectURL(blobImage);
   
     const anchorElement = document.createElement('a');
@@ -76,7 +88,6 @@ async function downloadImage(
   
 
 function getDoggie(){
-    debugger;
     var showFromAPI = getRandomInt(2);
 
     if (showFromAPI == 1) {
@@ -88,18 +99,25 @@ function getDoggie(){
         });
     }
     else{
-      var randomLocalImage = getRandomInt(localDoggiesPhotos.length)
-    //   document.getElementById("doggie").src = localDoggiesPhotos[randomLocalImage];
-    //https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image#Example_Storing_an_image_from_a_foreign_origin
-    let imageURL = localDoggiesPhotos[randomLocalImage];
-    let imageDescription = "The Mozilla logo";
 
-    downloadedImg = new Image();
-    downloadedImg.crossOrigin = "anonymous";
-    //   downloadedImg.addEventListener("load", imageReceived, false);
-    downloadedImg.alt = imageDescription;
-    downloadedImg.src = imageURL;
-    document.getElementById("doggie").src = downloadedImg.src;
+    debugger;
+    try {
+    var randomLocalImage = getRandomInt(localDoggiesPhotos.length)
+    document.getElementById("doggie").src = localDoggiesPhotos[randomLocalImage];
+    }
+    catch(ex){
+        console.log(ex);
+    }
+    // //https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image#Example_Storing_an_image_from_a_foreign_origin
+    // let imageURL = localDoggiesPhotos[randomLocalImage];
+    // let imageDescription = "The Mozilla logo";
+
+    // downloadedImg = new Image();
+    // downloadedImg.crossOrigin = "anonymous";
+    // //   downloadedImg.addEventListener("load", imageReceived, false);
+    // downloadedImg.alt = imageDescription;
+    // downloadedImg.src = imageURL;
+    // document.getElementById("doggie").src = downloadedImg.src;
 
     }
 }
@@ -143,7 +161,6 @@ function getInMinutes (){
 }
 
 function getYears(){
-    debugger;
     var startDateYear = _startDate.getFullYear();
     var currentYear = new Date().getFullYear();
 
@@ -159,15 +176,17 @@ function getYears(){
     return difference;
 }
 
+function changeMode(){
+    _newCountingMode = !_newCountingMode;
+}
+
 function updateSeconds()
 {
     putDaysWithoutAlcohol();
 }
 
-
 putDaysWithoutAlcohol();
 getDoggie();
 
 
-debugger;
 setInterval(updateSeconds, 1000);
